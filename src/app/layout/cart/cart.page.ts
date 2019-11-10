4092edd2e719ff97096c7b2d96a27909a2d06c56
rupamhazra <rupamhazra@gmail.com>
2019-11-11 00:08:44 +0530

    modified

--- src/app/layout/cart/cart.page.ts
+++ src/app/layout/cart/cart.page.ts
@@ -7,71 +7,70 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.page.html',
-  styleUrls: ['./cart.page.scss','../layout.page.scss'],
+  styleUrls: ['./cart.page.scss', '../layout.page.scss'],
 })
 export class CartPage implements OnInit {
-  eachProductPrice:any=0.00
-  totalPrice:any = 0.00;
-  productList:any;
-  selectedValue:any = "1" ;
-  media_url:any = environment.imageURL
-  products_exits_on_cart:boolean;
+  eachProductPrice: any = 0.00
+  totalPrice: any = 0.00;
+  productList: any;
+  selectedValue: any = "1";
+  media_url: any = environment.imageURL
+  products_exits_on_cart: boolean;
   constructor(
     private storage: Storage,
     public events1: Events,
-    private router : Router,
+    private router: Router,
   ) { }
 
   ngOnInit() {
-    console.log('medie_url',this.media_url)
+    console.log('medie_url', this.media_url)
     this.storage.get('allProductDetailsInCart').then((val) => {
-      console.log('val',val)
-      if(val) 
-      {
+      console.log('val', val)
+      if (val) {
         this.products_exits_on_cart = true
         this.productList = val
-        
-        val.forEach(element=>{
-          console.log('element',element.unit_price)
+
+        val.forEach(element => {
+          console.log('element', element.unit_price)
           this.totalPrice = parseFloat(this.totalPrice) + parseFloat(element.unit_price)
           this.eachProductPrice = element.unit_price
         });
-      }else{
-        console.log('val',val)
+      } else {
+        console.log('val', val)
         this.products_exits_on_cart = false
-        console.log('products_exits_on_cart',this.products_exits_on_cart)
+        console.log('products_exits_on_cart', this.products_exits_on_cart)
         this.storage.remove("allProductDetailsInCart");
       }
     });
   }
-  removeFromCartEvent(product_id:BigInteger){
-    console.log('this.productList.length',this.productList.length)
-    console.log('before',this.productList)
-      this.productList.forEach((item, key) => {
-        if(item.id == product_id) {
-          this.productList.pop(item)
-        }      
-      });
-      //console.log('after',this.productList)
-      if(this.productList.length>0){
-        console.log('exist cart')
-        this.storage.set("allProductDetailsInCart",this.productList);
-        //this.events1.publish('showProductCountOnCart',this.productList.length);
-      }else{ 
+  removeFromCartEvent(product_id: BigInteger) {
+    console.log('this.productList.length', this.productList.length)
+    console.log('before', this.productList)
+    this.productList.forEach((item, key) => {
+      if (item.id == product_id) {
+        this.productList.pop(item)
+      }
+    });
+    //console.log('after',this.productList)
+    if (this.productList.length > 0) {
+      console.log('exist cart')
+      this.storage.set("allProductDetailsInCart", this.productList);
+      //this.events1.publish('showProductCountOnCart',this.productList.length);
+    } else {
       //console.log('empty cart')
       this.products_exits_on_cart = false
-      console.log('products_exits_on_cart',this.products_exits_on_cart)
+      console.log('products_exits_on_cart', this.products_exits_on_cart)
       this.storage.remove("allProductDetailsInCart");
     }
-    this.events1.publish('showProductCountOnCart',this.productList.length);
+    this.events1.publish('showProductCountOnCart', this.productList.length);
   }
-  go_to_home(){
+  go_to_home() {
     this.router.navigateByUrl('/');
   }
-  placeOrderEvent(){
+  placeOrderEvent() {
     console.log('placeOrderEvent')
   }
-  changeQty(product_id){
-    console.log('product_id',product_id)
+  changeQty(product_id) {
+    console.log('product_id', product_id)
   }
 }
--- src/app/layout/home/home.page.ts
+++ src/app/layout/home/home.page.ts
@@ -12,69 +12,72 @@ import { SpeechRecognition } from '@ionic-native/speech-recognition/ngx';
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
-  styleUrls: ['./home.page.scss','../layout.page.scss'],
+  styleUrls: ['./home.page.scss', '../layout.page.scss'],
 })
 export class HomePage implements OnInit {
+  isItemAvailable: boolean;
+  search_items: any = ["Ram", "gopi", "dravid"];
   color: string = 'green';
-  name:any;
-  title:any;
-  medie_url:any = environment.imageURL
+  name: any;
+  title: any;
+  medie_url: any = environment.imageURL
   visibleKey: boolean = false;
-  result:[];
-  result_cat:[];
-  result_slide:[];
+  result: [];
+  result_cat: [];
+  result_slide: [];
   slideOpts = {
     initialSlide: 1,
     speed: 400,
-    autoplay:true
+    autoplay: true
   };
   slideOpts1 = {
     initialSlide: 1,
     speed: 400,
     slidesPerView: 4,
 
-    
+
   };
+
   constructor(
     private storage: Storage,
     private router: Router,
-    public loadingService : LoadingService,
+    public loadingService: LoadingService,
     private productService: ProductService,
     private toasterService: ToasterService,
     private categoryService: CategoryService,
     private sliderService: SliderService,
     private speechRecognition: SpeechRecognition
-    
+
   ) { }
 
   ngOnInit() {
-    
+
     /**
      * Speech recoganisation
      */
     // Check feature available
     this.speechRecognition.isRecognitionAvailable().then((available: boolean) => console.log(available))
-    
+
     this.speechRecognition.hasPermission()
       .then((hasPermission: boolean) => {
 
         if (!hasPermission) {
-        this.speechRecognition.requestPermission()
-          .then(
-            () => console.log('Granted'),
-            () => console.log('Denied')
-          )
+          this.speechRecognition.requestPermission()
+            .then(
+              () => console.log('Granted'),
+              () => console.log('Denied')
+            )
         }
 
-     });
+      });
 
-    
+
     //console.log('this.router.url', this.router.url);
     this.title = this.router.url;
     this.storage.get('USER_INFO').then((val) => {
 
-      if(val) this.name = val.name
-      
+      if (val) this.name = val.name
+
     });
     //this.loadingService.present();
     this.readSliders();
@@ -82,23 +85,11 @@ export class HomePage implements OnInit {
     this.readProducts();
     //this.loadingService.dismiss();
   }
-  start() {
-    
-    this.speechRecognition.startListening()
-      .subscribe(
-        (matches: Array<string>) => {
-          console.log('matches',matches)
-          this.color = matches[0];
-        },
-        (onerror) => console.log('error:', onerror)
-      )
 
-}
-  readSliders()
-  {
+  readSliders() {
     this.loadingService.present();
     //this.categories =[];
-    this.sliderService.readSliders().subscribe( 
+    this.sliderService.readSliders().subscribe(
       res => {
         //this.result = res.result;
         this.result_slide = res.result;
@@ -107,41 +98,39 @@ export class HomePage implements OnInit {
         //console.log("afterrrrrrrrrrrr");
         this.visibleKey = true;
       },
-    error => {
-      console.log("error::::"+error);
-      this.loadingService.dismiss();
-      this.visibleKey = true;
-      this.toasterService.showToast(error.error.msg,2000)
+      error => {
+        console.log("error::::" + error);
+        this.loadingService.dismiss();
+        this.visibleKey = true;
+        this.toasterService.showToast(error.error.msg, 2000)
 
-    }
+      }
     )
   }
-  readCategories()
-  {
+  readCategories() {
     //this.loadingService.present();
     //this.categories =[];
-    this.categoryService.readCategories().subscribe( 
+    this.categoryService.readCategories().subscribe(
       res => {
         this.result_cat = res.result;
         //console.log("result_cat",this.result_cat);
-       // this.loadingService.dismiss();
+        // this.loadingService.dismiss();
         //console.log("afterrrrrrrrrrrr");
         this.visibleKey = true;
       },
-    error => {
-      console.log("error::::"+error);
-      //this.loadingService.dismiss();
-      this.visibleKey = true;
-      this.toasterService.showToast(error.error.msg,2000)
+      error => {
+        console.log("error::::" + error);
+        //this.loadingService.dismiss();
+        this.visibleKey = true;
+        this.toasterService.showToast(error.error.msg, 2000)
 
-    }
+      }
     )
   }
-  readProducts()
-  {
+  readProducts() {
     //this.loadingService.present();
     //this.categories =[];
-    this.productService.readProducts().subscribe( 
+    this.productService.readProducts().subscribe(
       res => {
         this.result = res.result;
         //this.result_slide = res.result;
@@ -150,18 +139,47 @@ export class HomePage implements OnInit {
         //console.log("afterrrrrrrrrrrr");
         this.visibleKey = true;
       },
-    error => {
-      console.log("error::::"+error);
-      //this.loadingService.dismiss();
-      this.visibleKey = true;
-      this.toasterService.showToast(error.error.msg,2000)
+      error => {
+        console.log("error::::" + error);
+        //this.loadingService.dismiss();
+        this.visibleKey = true;
+        this.toasterService.showToast(error.error.msg, 2000)
 
-    }
+      }
     )
   }
-  getProduct(id){
+  getProduct(id) {
     //console.log('id====',id)
-    this.router.navigateByUrl('/products/product-single/'+id);
+    this.router.navigateByUrl('/products/product-single/' + id);
   }
+  getItems(ev: any) {
+    // set val to the value of the searchbar
+    const val = ev.target.value;
+    // if the value is an empty string don't filter the items
+    if (val && val.trim() != '') {
+      this.isItemAvailable = true;
+      this.search_items = this.search_items.filter((item) => {
+        return (item.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      })
+    }
+  }
+  start() {
+    console.log('sdsdsdsdds')
 
+    this.speechRecognition.startListening()
+      .subscribe(
+
+        (matches: Array<string>) => {
+          console.log('matches', matches)
+          this.isItemAvailable = true;
+          this.search_items = this.search_items.filter((item) => {
+            console.log('item', item)
+            return (item.toLowerCase().indexOf(matches[0].toLowerCase()) > -1);
+          })
+
+          //this.color = matches[0];
+        },
+        (onerror) => console.log('error:', onerror)
+      )
+  }
 }
